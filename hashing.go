b77125a19db96187be1c06111cddd19c109425d3
AureumApes <janundartur@gmail.com>
2023-03-21 18:04:33 +0100

    Fixed hashing

--- hashing.go
+++ hashing.go
@@ -1,5 +1,9 @@
 package goutil
 
-func Hash() {
+import (
+	"crypto/sha256"
+)
 
+func Hash(text string, salt string, salt2 string) string {
+	return string(sha256.New().Sum([]byte(salt + text + salt2)))
 }
